ac0b19aef838dc01f1beccd08270a2bec62e948c
Spoon <nycbasic@gmail.com>
2023-01-24 17:03:26 -0800

    Updates

---
+++ precache-manifest.9a991cd336955de101c706b10923ca05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f554e658fe091f526a56",
+    "url": "/mini-auth/static/css/main.ec028bf7.chunk.css"
+  },
+  {
+    "revision": "f554e658fe091f526a56",
+    "url": "/mini-auth/static/js/main.f554e658.chunk.js"
+  },
+  {
+    "revision": "f8f6c879c95690ae50be",
+    "url": "/mini-auth/static/css/1.d82232ba.chunk.css"
+  },
+  {
+    "revision": "f8f6c879c95690ae50be",
+    "url": "/mini-auth/static/js/1.f8f6c879.chunk.js"
+  },
+  {
+    "revision": "5bea8be4c346aaa07f6c",
+    "url": "/mini-auth/static/js/runtime~main.5bea8be4.js"
+  },
+  {
+    "revision": "67cc336ec7521e20405fe12c1d6f5611",
+    "url": "/mini-auth/index.html"
+  }
+];
